eb50de724fcf524e77d4be2c6bcd2852efedbf11
Johnson Kwok <johnsonkwok1@gmail.com>
2019-01-02 18:25:45 -0800

    Change formatting of quotation marks in index.html file that was causing errors

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = 3100;
 
 app.use('/rooms/:id', express.static(path.join(__dirname, 'public')));
 
