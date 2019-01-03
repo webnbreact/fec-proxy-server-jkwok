10d8a7e22527f1100e30bfb690200ba0ca98564c
Johnson Kwok <johnsonkwok1@gmail.com>
2019-01-02 17:14:07 -0800

    Set up proxy server

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const morgan = require('morgan');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/rooms/:id', express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
-  console.log(`Server running at: http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });
