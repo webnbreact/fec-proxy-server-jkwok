1f071732f92051f95ecd58b5eabc174c5bf2e9c2
Johnson Kwok <johnsonkwok1@gmail.com>
2019-01-02 17:14:35 -0800

    Add booking checkout module

---
+++ modules/checkout/server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const cors = require('cors');
+const { getRoomBookingData } = require('./controllers.js');
+
+const app = express();
+
+const mainPagePath = path.join(__dirname, '/../public');
+app.use(express.static(mainPagePath));
+app.use(morgan('tiny'));
+app.use(cors());
+
+// app.use('/rooms/:id/', express.static(mainPagePath));
+app.use('/rooms/', express.static(mainPagePath));
+
+app.get('/rooms/:id/booking', (req, res) => {
+  const roomId = req.params.id;
+  getRoomBookingData(roomId)
+    .then((roomInfo) => {
+      res.send(roomInfo);
+    }).catch((err) => {
+      console.error(err);
+      res.sendStatus(501);
+    });
+});
+
+const port = 1337;
+app.listen(port, () => console.log(`Server running on port ${port}`));
