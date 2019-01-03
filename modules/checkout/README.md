62029db92e4398922d95cec642e04bc52a701ddd
Johnson Kwok <johnsonkwok1@gmail.com>
2019-01-02 23:00:38 -0800

    Incorporate Product Description module into proxy server

--- modules/checkout/README.md
+++ modules/checkout/README.md
@@ -10,7 +10,7 @@
   - https://github.com/webnbreact/carousel
 
 ## Usage Instructions
-1) Run "npm install --only=prod" to install dependencies without dev-dependencies.
+1) Run "npm install" to install dependencies.
 2) Install Nodemon globally if it hasn't already been installed. ("npm install nodemon -g")
 3) Run "npm run seed-db" (Note: You may have to change the seed-db script and add "-p" if your mysql root user has a password. This may also require adding the line "password: [yourPassword]" to line 6 of 'db/seedDb.js'.)
 4) Run "npm start".
--- modules/product_description/server/index.js
+++ modules/product_description/server/index.js
@@ -9,12 +9,12 @@ const port = process.env.PORT || 3003;
 const {Listings} = require('../database/seedListing.js');
 // const {Amenities} = require('../database/seedAmenities.js');
 
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 
-app.use(express.static(path.join(__dirname, '/../client/dist')));
+app.use('/rooms/', express.static(path.join(__dirname, '/../client/dist')));
 
 app.get('/rooms/:id/listings', (req, res) => {
   // console.log(`this is req.params`, req.params.id);
