fe2e88e0ef85773fbbda4cdccdf8cc4b7d96e91f
Johnson Kwok <johnsonkwok1@gmail.com>
2019-01-03 21:09:47 -0800

    Add updated Carousel module

--- modules/carousel/server/index.js
+++ modules/carousel/server/index.js
@@ -8,10 +8,11 @@ const { urlencoded, json } = bodyParser;
 const app = express();
 
 app.use(morgan('dev'));
-app.use(urlencoded({ extended: true }));
+app.use(urlencoded({ extended: false }));
 app.use(json());
 
-app.use(express.static('public'));
+app.use('/rooms/:id/', express.static(path.resolve(__dirname, '../public')));
+
 
 app.get('/rooms/:id/pictures', (req, res) => {
   findPicturesByRoomId(req.params.id)
@@ -21,9 +22,9 @@ app.get('/rooms/:id/pictures', (req, res) => {
     });
 });
 
-app.get('/:id', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../index.html'));
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, '../index.html'));
+// });
 
 app.listen(4500, () => {
   console.log('You\'ve sucessfully connected!');
