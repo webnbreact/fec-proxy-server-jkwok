23a6870e573830c63c5ab75288b9f2811f598565
Johnson Kwok <johnsonkwok1@gmail.com>
2019-01-05 13:43:49 -0800

    Add placeholders for other modules

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const compression = require('compression');
+const compression = require('compression');
 const path = require('path');
 const morgan = require('morgan');
 const proxy = require('http-proxy-middleware');
@@ -7,11 +7,16 @@ const proxy = require('http-proxy-middleware');
 const app = express();
 const port = 3100;
 
-// app.use(compression());
+app.use(compression());
 app.use(morgan('tiny'));
 
 app.use('/rooms/:id', express.static(path.join(__dirname, 'public')));
 app.use('/rooms/:id/booking', proxy({ target: 'http://18.224.7.253/', changeOrigin: true }));
+// app.use('/rooms/:id/listings', proxy({ target: 'http://XXXXX/', changeOrigin: true }));
+// app.use('/rooms/:id/pictures', proxy({ target: 'http://XXXXX/', changeOrigin: true }));
+// app.use('/rooms/:id/stars', proxy({ target: 'http://XXXXX/', changeOrigin: true }));
+// app.use('/rooms/:id/hostDetails', proxy({ target: 'http://XXXXX/', changeOrigin: true }));
+// app.use('/rooms/:id/reviews', proxy({ target: 'http://XXXXX/', changeOrigin: true }));
 
 
 app.listen(port, () => {
