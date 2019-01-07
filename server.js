0a62d15e3528df1e92f645e33cab24202c148509
Johnson Kwok <johnsonkwok1@gmail.com>
2019-01-06 18:35:58 -0800

    Update links to other modules

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ app.use(compression());
 app.use(morgan('tiny'));
 
 app.use('/rooms/:id', express.static(path.join(__dirname, 'public')));
-app.use('/rooms/:id/booking', proxy({ target: 'http://18.224.7.253/', changeOrigin: true }));
-// app.use('/rooms/:id/listings', proxy({ target: 'http://XXXXX/', changeOrigin: true }));
-// app.use('/rooms/:id/pictures', proxy({ target: 'http://XXXXX/', changeOrigin: true }));
-// app.use('/rooms/:id/stars', proxy({ target: 'http://XXXXX/', changeOrigin: true }));
-// app.use('/rooms/:id/hostDetails', proxy({ target: 'http://XXXXX/', changeOrigin: true }));
-// app.use('/rooms/:id/reviews', proxy({ target: 'http://XXXXX/', changeOrigin: true }));
+app.use('/rooms/:id/booking', proxy({ target: 'http://ec2-18-224-7-253.us-east-2.compute.amazonaws.com/', changeOrigin: true }));
+app.use('/rooms/:id/listings', proxy({ target: 'http://ec2-13-59-45-120.us-east-2.compute.amazonaws.com/', changeOrigin: true }));
+app.use('/rooms/:id/pictures', proxy({ target: 'http://ec2-18-221-26-182.us-east-2.compute.amazonaws.com/', changeOrigin: true }));
+app.use('/rooms/:id/stars', proxy({ target: 'http://ec2-18-218-142-121.us-east-2.compute.amazonaws.com/', changeOrigin: true }));
+app.use('/rooms/:id/hostDetails', proxy({ target: 'http://ec2-18-218-142-121.us-east-2.compute.amazonaws.com/', changeOrigin: true }));
+app.use('/rooms/:id/reviews', proxy({ target: 'http://ec2-18-218-142-121.us-east-2.compute.amazonaws.com/', changeOrigin: true }));
 
 
 app.listen(port, () => {
